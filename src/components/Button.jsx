d8da1a2141bf8cbc79b42a2928043403b3d3e94c
Ferinco <emmanuelferrum003@gmail.com>
2024-09-19 18:05:52 +0100

    login page done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 export const Button = (props)=> {
     return(
-        <button className={`px-5 py-2 rounded-xl border h-[50.49px] text-base font-semibold ${props.type === "deep" ? "bg-custom-blue text-white border-custom-blue" : "bg-transparent text-custom-blue border-custom-blue"} ${props.full ? "w-full" : "w-fit" }`}>{props.title}</button>
+        <button className={`px-5 py-2 rounded-lg border h-[50.49px] text-base font-semibold shadow-custom ${props.deep ? "bg-custom-blue text-white border-custom-blue" : "bg-transparent text-custom-blue border-custom-blue"} ${props.full ? "w-full" : "w-fit" }`}>{props.title}</button>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,20 +1,47 @@
+import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 import { FormInput } from "../components/Input";
 
 export default function LoginPage() {
   return (
     <div className="grid grid-cols-12 h-screen w-screen">
-      <div className="h-full col-span-8 bg-[url('/images/left-banner.png')] bg-cover bg-center bg-no-repeat">
-      <div className="h-full w-full">
+      <div className="h-full hidden md:block md:col-span-7 lg:col-span-8 bg-[url('/images/left-banner.png')] bg-cover bg-center bg-no-repeat">
+        <div className="h-full w-full"></div>
       </div>
-      </div>
-      <div className="h-full col-span-4 px-7 flex flex-col justify-center">
+      <div className="h-full col-span-12  md:col-span-5 lg:col-span-4 px-7 sm:px-16 md:px-7 flex flex-col justify-center gap-7">
         <p className="text-custom-grey font-semibold text-base text-center text-shadow-custom">
           Login into your account
         </p>
-        <FormInput placeholder="info@pearmonie.test" name="email" type="email" label="Email Id:" icon="/images/mail-icon.png"/>
-        <FormInput placeholder="Enter your password" name="password" type="password" label="Password" icon="/images/password-icon.png"/>
-        <Button deep title="Login now" full/>
+        <div className="flex flex-col gap-4 mt-8">
+
+        <FormInput
+          placeholder="info@pearmonie.test"
+          name="email"
+          type="email"
+          label="Email Id:"
+          icon="/images/mail-icon.png"
+        />
+        <FormInput
+          placeholder="Enter your password"
+          name="password"
+          type="password"
+          label="Password"
+          icon="/images/password-icon.png"
+        />
+        <a className="text-custom-blue underline text-sm font-normal flex self-end" href="/">
+          Forgot password?
+        </a>
+        </div>
+        <div className="flex flex-col">
+        <Button deep title="Login now" full />
+        <div className="flex flex-col relative items-center my-8">
+          <div className="w-full line border-t border-black/25 shadow-custom"></div>
+          <p className="text-black/25 font-light text-sm text-shadow-custom w-fit px-4 absolute -mt-[9px] bg-white">
+            OR
+          </p>
+        </div>
+        <Button title="Signup now" full />
+        </div>
       </div>
     </div>
   );
